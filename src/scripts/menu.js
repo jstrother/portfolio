22d9626651e9a20b3e9e0d1ab9c8a67add11284c
Jim <jim.strother@gmail.com>
2023-11-03 15:14:56 -0500

    Adjusting layout

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,6 @@
 document.addEventListener('astro:page-load', () => {
   document.querySelector('.hamburger').addEventListener('click', () => {
+    document.querySelector('#themeToggle').classList.toggle('hidden');
     document.querySelector('.nav-links').classList.toggle('expanded');
   });
 });
